8593ab7dde4f54f5a2b0203e7e509d35333b50e8
Roma <mig29.romakos@mail.ru>
2022-10-16 12:52:38 +0600

    Log class

---
+++ Astana/Class/Log/LogClass.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+using System.IO;
+
+namespace Astana.Class.Log
+{
+    public static class LogClass
+    {
+        static object loc = new object();
+        public static void Write(string logText)
+        {
+            lock (loc)
+            {
+                using (StreamWriter sw = new StreamWriter("Log.txt", true, Encoding.UTF8))
+                {
+                    try
+                    {
+                        sw.WriteLine(logText);
+                    }
+                    catch { }
+                }
+            }
+        }
+    }
+}
